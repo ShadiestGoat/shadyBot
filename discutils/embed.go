6170f9b79db0d854ca4c73d43155285443ad3f23
ShadiestGoat <48590492+ShadiestGoat@users.noreply.github.com>
2023-05-19 00:55:28 +0100

    FUck you <3

--- discutils/embed.go
+++ discutils/embed.go
@@ -16,7 +16,7 @@ const (
 var BaseEmbed = discordgo.MessageEmbed{
 	Title: EMBED_EMPTY,
 	Footer: &discordgo.MessageEmbedFooter{
-		Text:    "Made By Shady Goat & hello_world",
+		Text:    "Made By Shady Goat",
 		IconURL: "https://media.discordapp.net/attachments/735886276070342696/1020345904164773918/pfp-f.png",
 	},
 	Fields: []*discordgo.MessageEmbedField{},
